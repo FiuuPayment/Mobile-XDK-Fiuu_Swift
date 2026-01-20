bd89d673d5113a750fd4d23570439b327c4dd77b
beastback <hainizam.abdullah@fiuu.com>
2026-01-20 12:54:19 +0800

    moved xcframework file

--- Package.swift
+++ Package.swift
@@ -5,20 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "Mobile-XDK-Fiuu_Swift",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Mobile-XDK-Fiuu_Swift",
-            targets: ["Mobile-XDK-Fiuu_Swift"]),
+            targets: ["FiuuXDKSwift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Mobile-XDK-Fiuu_Swift"),
-        .testTarget(
-            name: "Mobile-XDK-Fiuu_SwiftTests",
-            dependencies: ["Mobile-XDK-Fiuu_Swift"]
-        ),
+        .binaryTarget(name: "FiuuXDKSwift", path: "FiuuXDKSwift.xcframework"),
     ]
 )
